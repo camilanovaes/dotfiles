3e999e8d5b5a61f66c262b160705e0dc80bc4d9a
Camila Novaes <novaes.csw@gmail.com>
2021-10-12 18:04:34 -0300

    Use cmp instead of compe as completation plugin

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,5 +1,7 @@
 -- Enable language server
-require'lspconfig'.pyright.setup{}
+require'lspconfig'.pyright.setup{
+   capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+}
 
 
 local nvim_lsp = require('lspconfig')
