9305ffe4b567a7eeaa2e509d618039ff85c81c89
Camila Novaes <novaes.csw@gmail.com>
2021-10-12 16:24:56 -0300

    Add telescope configurations

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -15,7 +15,7 @@ require('telescope').setup{
     initial_mode = "insert",
     selection_strategy = "reset",
     sorting_strategy = "descending",
-    layout_strategy = "horizontal",
+    layout_strategy = "vertical",
     layout_config = {
       horizontal = {
         mirror = false,
@@ -37,9 +37,41 @@ require('telescope').setup{
     file_previewer = require'telescope.previewers'.vim_buffer_cat.new,
     grep_previewer = require'telescope.previewers'.vim_buffer_vimgrep.new,
     qflist_previewer = require'telescope.previewers'.vim_buffer_qflist.new,
-
+	
     -- Developer configurations: Not meant for general override
     buffer_previewer_maker = require'telescope.previewers'.buffer_previewer_maker
-  }
+  },
+	pickers = {
+		find_files = {
+			theme = "ivy",
+			disable_devicons = true,
+			previewer = false,
+			winblend = 10,
+			layout_config = {
+				height = 0.4
+			}
+		},
+		live_grep = {
+			theme = "ivy",
+			disable_devicons = true,
+			winblend = 10,
+			layout_config = {
+				height = 0.4
+			}
+
+		},
+		grep_string = {
+			theme = "ivy",
+			disable_devicons = true
+		},
+		buffers = {
+			theme = "ivy",
+			disable_devicons = true
+		},
+		help_tags = {
+			theme = "ivy",
+			disable_devicons = true
+		}
+	}
 }
 
