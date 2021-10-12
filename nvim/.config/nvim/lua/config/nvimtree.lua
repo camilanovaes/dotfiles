3435bfcde0ebe17e871f5abd9d3f4ac15abb810f
Camila Novaes <novaes.csw@gmail.com>
2021-10-12 16:46:50 -0300

    Update nvimtree configurations

--- nvim/.config/nvim/lua/config/nvimtree.lua
+++ nvim/.config/nvim/lua/config/nvimtree.lua
@@ -1,38 +1,43 @@
 -- nvim-tree
-
-local g = vim.g
-
-g.nvim_tree_width = 40
-g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_auto_close = 1
-g.nvim_tree_auto_ignore_ft = { 'minimap' }
-g.nvim_tree_follow = 1
-
-g.nvim_tree_show_icons = {
-	git = 0,
-	folders = 1,
-	files = 1,
-	folder_arrows = 1,
-}
-
-g.nvim_tree_window_picker_exclude = {
-	filetype = {
-		'minimap',
-		'packer',
-		'qf',
-	},
-	buftype = {
-		'terminal',
-	},
-}
-
-g.nvim_tree_ignore = {
-	'.DS_Store',
-	'.cache',
-	'.git',
-	'.idea',
-	'.mypy_cache',
-	'.vagrant',
-	'__pycache__',
-	'node_modules',
+require('nvim-tree').setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  update_to_buf_dir   = {
+	enable = true,
+	auto_open = true,
+  },
+  auto_close          = false,
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  diagnostics         = {
+	enable = false,
+	icons = {
+	  hint = "",
+	  info = "",
+	  warning = "",
+	  error = "",
+	}
+  },
+  update_focused_file = {
+	enable      = true,
+	update_cwd  = false,
+	ignore_list = {}
+  },
+  system_open = {
+	cmd  = nil,
+	args = {}
+  },
+  view = {
+	width = 30,
+	height = 30,
+	side = 'left',
+	auto_resize = false,
+	mappings = {
+	  custom_only = false,
+	  list = {}
+	}
+  },
 }
