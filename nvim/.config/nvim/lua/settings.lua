5cda5746b0edde3693dc4b9e864cc62753eb51b7
Camila Novaes <novaes.csw@gmail.com>
2022-02-13 13:53:22 -0300

    Cosmetics change on nvim's settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -6,24 +6,24 @@ local wo = vim.wo
 local bo = vim.bo
 
 -- global options
-o.mouse           = "a"
-o.inccommand      = "nosplit"
-o.background      = "dark"
-vim.o.completeopt = "menuone,noselect"
-o.swapfile        = false
-o.smartcase       = true
-o.laststatus      = 2
-o.incsearch		  = true
-o.hlsearch        = false
-o.ignorecase      = true
-o.errorbells      = false
-o.scrolloff       = 8
-o.smarttab        = true
-o.backup          = false
-o.splitbelow      = true
-o.splitright      = true
-o.showmode        = false
-o.termguicolors   = true
+o.mouse         = "a"
+o.inccommand    = "nosplit"
+o.background    = "dark"
+o.completeopt   = "menuone,noselect"
+o.swapfile      = false
+o.smartcase     = true
+o.laststatus	= 2
+o.incsearch     = true
+o.hlsearch      = false
+o.ignorecase    = true
+o.errorbells    = false
+o.scrolloff     = 8
+o.smarttab      = true
+o.backup        = false
+o.splitbelow    = true
+o.splitright    = true
+o.showmode      = false
+o.termguicolors = true
 
 -- window-local options
 wo.number         = true
