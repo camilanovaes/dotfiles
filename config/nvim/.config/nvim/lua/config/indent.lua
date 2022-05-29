8928a8c8aa6c66d050e0476a0feb9329ccdd341d
Camila Novaes <novaes.csw@gmail.com>
2022-05-29 15:52:33 -0300

    Add indent-blankline configuration

--- config/nvim/.config/nvim/lua/config/indent.lua
+++ config/nvim/.config/nvim/lua/config/indent.lua
@@ -1,6 +1,12 @@
 -- Indent blanklines config
 --
-require("indent_blankline").setup{
-  char = "|",
-  buftype_exclude = {"terminal"}
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
 }
+
