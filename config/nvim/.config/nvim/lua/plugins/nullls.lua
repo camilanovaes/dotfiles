30d817a142ba4de2e34d71f560981a1fcf1776e5
Camila Novaes <novaes.csw@gmail.com>
2023-05-29 17:46:39 -0300

    nvim: Add isort to null_ls

--- config/nvim/.config/nvim/lua/plugins/nullls.lua
+++ config/nvim/.config/nvim/lua/plugins/nullls.lua
@@ -1,6 +1,12 @@
-require("null-ls").setup({
-    sources = {
-        require("null-ls").builtins.formatting.yapf,
-        require("null-ls").builtins.diagnostics.flake8,
-    },
+-- Null LS configuration
+
+local null_ls = require("null-ls")
+local sources = {
+    null_ls.builtins.formatting.yapf,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.diagnostics.flake8,
+}
+
+null_ls.setup({
+    sources = sources,
 })
